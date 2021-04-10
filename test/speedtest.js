7bc3a9ca7d2e30cd2509b1fbad510434dd52f7e6
NiciusB <nuno@balbona.me>
2021-04-10 23:38:26 +0200

    Misc code cleanup

--- test/speedtest.js
+++ test/speedtest.js
@@ -1,14 +1,14 @@
-const imageHash = require('./index')
+const imageHash = require('../index')
 const precise = require('precise')
 const path = require('path')
 
-async function test () {
+async function speedtest () {
   console.log('Calculating async times')
   let hashes = []
   let timer = precise()
   timer.start()
   for (let k = 0; k < 500; k++) {
-    hashes.push(imageHash.hash(path.join(__dirname, 'test', 'files', 'castle2.png')))
+    hashes.push(imageHash.hash(path.join(__dirname, 'files', 'castle2.png')))
   }
   await Promise.all(hashes).then(() => {
     timer.stop()
@@ -20,7 +20,7 @@ async function test () {
   timer = precise()
   timer.start()
   for (let k = 0; k < 500; k++) {
-    hashes.push(imageHash.syncHash(path.join(__dirname, 'test', 'files', 'castle2.png')))
+    hashes.push(imageHash.syncHash(path.join(__dirname, 'files', 'castle2.png')))
   }
   await Promise.all(hashes).then(() => {
     timer.stop()
@@ -30,4 +30,4 @@ async function test () {
   imageHash.close()
 }
 
-test()
+speedtest()
